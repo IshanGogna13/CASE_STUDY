a8053823f08bd050a1ad687a257becb8fc397cc2
Ishan Gogna <ishan.gogna@sonata-software.com>
2021-08-22 23:23:13 +0530

    Mock_UI

--- C#_test/Program.cs
+++ C#_test/Program.cs
@@ -4,35 +4,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp2
+namespace linqspace
 {
-    
-        
-    
-       
-        class UsingPeople                                                               //class using people to test program
+    public class Student
+    {
+        public string First { get; set; }
+        public string Last { get; set; }
+        public int ID { get; set; }
+        public List<int> Scores;
+        static List<Student> students = new List<Student>
+{
+    new Student {First="Svetlana", Last="Omelchenko", ID=111, Scores= new List<int> {97, 92, 81, 60}},
+    new Student {First="Claire", Last="O'Donnell", ID=112, Scores= new List<int> {75, 84, 91, 39}},
+    new Student {First="Sven", Last="Mortensen", ID=113, Scores= new List<int> {88, 94, 65, 91}},
+    new Student {First="Cesar", Last="Garcia", ID=114, Scores= new List<int> {97, 89, 85, 82}},
+    new Student {First="Debra", Last="Garcia", ID=115, Scores= new List<int> {35, 72, 91, 70}},
+    new Student {First="Fadi", Last="Fakhouri", ID=116, Scores= new List<int> {99, 86, 90, 94}},
+    new Student {First="Hanying", Last="Feng", ID=117, Scores= new List<int> {93, 92, 80, 87}},
+    new Student {First="Hugo", Last="Garcia", ID=118, Scores= new List<int> {92, 90, 83, 78}},
+    new Student {First="Lance", Last="Tucker", ID=119, Scores= new List<int> {68, 79, 88, 92}},
+    new Student {First="Terry", Last="Adams", ID=120, Scores= new List<int> {99, 82, 81, 79}},
+    new Student {First="Eugene", Last="Zabokritski", ID=121, Scores= new List<int> {96, 85, 91, 60}},
+    new Student {First="Michael", Last="Tucker", ID=122, Scores= new List<int> {94, 92, 91, 91}}
+};
+    }
+
+    static void Main(string[] args)
         {
-        public static void Main()
-        { 
-            List<string> ski = new List<string> ( );                                    //list of skills
-            ski.Add("C++");
-            ski.Add("Java");
-            Employee emp;
-            emp = new TechnicalEmployee(123, "avi", "balrampur", 13345,ski );
-           
-            Console.WriteLine($"Employee Id is :{emp.EmpId}");
-            Console.WriteLine($"Employee Name is :{emp.EmpName}");
-            Console.WriteLine($"Employee Salary is :{emp.calculateSalary()}");
-            Employee empp;
-            
-            empp = new Staff(4566, "Ishan", "Gurgaon", 50000, "hii");
-          
-            Console.WriteLine($"Employee : {empp.ToString()}");
-            Console.WriteLine($"Employee salary is: {empp.calculateSalary()}");
+        //string[] names = { "Ishan", "Advaith", "Mary", "Zaki", "Sathya", "Ishi", "Aarush" };
+        //var Query = from n in names
+        //            group n by n[0];
 
 
 
-        }
+        //foreach (var q in Query)
+        //{
+        //    Console.WriteLine(q.Key + " , " + q.Count());
+        //    foreach (var r in q)
+        //    {
+        //        Console.WriteLine(r);
+        //    }
+        //}
+        IEnumerable<Student> studentQuery =
+        from student in students
+         where student.Scores[0] > 90
+         select student;
 
-        }
+    }
+    
 }
